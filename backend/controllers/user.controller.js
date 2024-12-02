25300e7e34f57de87ce745d94d5c68ca1e0add7f
monishkh <monishkhan2409@gmail.com>
2024-12-02 09:26:01 +0530

    bcrypt-js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { User } from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs'
 import jwt from "jsonwebtoken";
 import getDataUri from "../utils/datauri.js";
 import cloudinary from "../utils/cloudinary.js";
