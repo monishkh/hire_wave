b4cb0a0b6caea9cb00582fd2c0a615a2761a8263
monishkh <monishkhan2409@gmail.com>
2024-12-02 15:03:20 +0530

    Url fix

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "https://hire-wave-lnpe.onrender.com/api/v1/user"
-export const JOB_API_END_POINT = "https://hire-wave-lnpe.onrender.com/api/v1/job"
-export const COMPANY_API_END_POINT = "https://hire-wave-lnpe.onrender.com/api/v1/company"
-export const APPLICATION_API_END_POINT = "https://hire-wave-lnpe.onrender.com/api/v1/application";
+export const USER_API_END_POINT = "https://hire-wave-85qi.onrender.com/api/v1/user"
+export const JOB_API_END_POINT = "https://hire-wave-85qi.onrender.com/api/v1/job"
+export const COMPANY_API_END_POINT = "https://hire-wave-85qi.onrender.com/api/v1/company"
+export const APPLICATION_API_END_POINT = "https://hire-wave-85qi.onrender.com/api/v1/application";
